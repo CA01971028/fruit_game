fafdfa91119a2c6377818a5dfd4fd3c1661302ad
CA01971028 <107534406+CA01971028@users.noreply.github.com>
2024-05-17 13:52:31 +0900

    Adjusting the merge

--- game/frontend/src/App.tsx
+++ game/frontend/src/App.tsx
@@ -3,23 +3,19 @@ import React from 'react';
 import './App.css';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Flask_account from './compornent/Flask_account';
-import Flask from './compornent/Flask';
+import Supot from './compornent/Supot'
 import { AuthProvider, useAuth } from './compornent/AuthContext';
 import './App.css'
 import './css/compornent.css'
-import Background from './compornent/background'
-import Score from './compornent/score'
-import Ranking from './compornent/ranking'
-import Next from './compornent/next'
-import Next_block from './compornent/next_block';
-import Rectangle from './compornent/rectangle';
+
+
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<Flask_account />} />
-        <Route path="/login" element={<ProtectedRoute component={Flask} />} />
+        <Route path="/login" element={<ProtectedRoute component={Supot} />} />
       </Routes>
     </AuthProvider>
   );
--- game/frontend/src/index.tsx
+++ game/frontend/src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Flask from './compornent/Flask'
-import Flask_account from './compornent/Flask_account';
 import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
