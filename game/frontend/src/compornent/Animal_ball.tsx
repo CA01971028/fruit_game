57078b00c322101f89514ccfe789de05b89549bc
CA01971148 <CA01971148@st.kawahara.ac.jp>
2024-07-08 12:24:51 +0900

    name

--- game/frontend/src/compornent/Animal_ball.tsx
+++ game/frontend/src/compornent/Animal_ball.tsx
@@ -1,3 +1,4 @@
+// Animal_ball.tsx
 import React, { useState, useEffect, useRef } from 'react';
 import { Hamster } from './Rectangle';
 
--- game/frontend/src/compornent/rectangle.tsx
+++ game/frontend/src/compornent/rectangle.tsx
@@ -1,3 +1,4 @@
+// Rectangle.tsx
 import React, { useState, useEffect } from 'react';
 import owlImage from '../img/owl.png';
 import ham1 from '../img/hamster.png';
